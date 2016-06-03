674c361effd45cac457f66eea0f944f4c590aca7
Your Name <you@example.com>
2016-06-03 15:47:22 +0100

    completed search function

--- specs/store_specs.js
+++ specs/store_specs.js
@@ -5,6 +5,14 @@ var assert = require('chai').assert
   beforeEach(function(){
     store1 = new Store("Record Breakers", "Edinburgh", 1000);
     record1 = new Record("tool", "lateralus", 18.00);
+    record2 = new Record("RATM", "renagades", 18.00);
+    record3 = new Record("korn", "follow the leader", 18.00);
+    record4 = new Record("a perfect circle", "thirteenth step", 18.00);
+    store1.add(record1);
+    store1.add(record2);
+    store1.add(record3);
+    store1.add(record4);
+
   }),
 
   it('has a name', function(){
@@ -20,7 +28,16 @@ var assert = require('chai').assert
   })
 
   it('adds a record to a store', function(){
-    store1.add(record1)
     assert.deepEqual({artist: "tool", title:  "lateralus", price: 18.00}, store1.records[0])
+  })
+
+  it('should be able to find a record', function(){
+    result = store1.find("renagades")
+    assert.deepEqual({artist: "RATM",title: "renagades",price: 18.00
+  }, result)
+  })
+
+  it('should be able to a record', function(){
+    store1.sell("renagades")
   })
 
--- store_constructor.js
+++ store_constructor.js
@@ -1,3 +1,6 @@
+var Record = require('./record_constructor')
+var _ = require('lodash');
+
 var Store = function(name, city, balance){
   this.name = name;
   this.city = city;
@@ -8,7 +11,29 @@ var Store = function(name, city, balance){
 Store.prototype = {
   add: function(record){
     this.records.push(record);
+  },
+  find: function(title){
+    return _.find(this.records, function(o) { return o.title === title; })
+  },
+  inventory: function(){
+    for (record of this.records) {
+      console.log(record)
+    } 
   }
+
+
+
 }
+// store1 = new Store("Record Breakers", "Edinburgh", 1000);
+// record1 = new Record("tool", "lateralus", 18.00);
+// record2 = new Record("RATM", "renagades", 18.00);
+// record3 = new Record("korn", "follow the leader", 18.00);
+// record4 = new Record("a perfect circle", "thirteenth step", 18.00);
+// store1.add(record1);
+// store1.add(record2);
+// store1.add(record3);
+// store1.add(record4);
+
+// store1.inventory()
 
 module.exports = Store;
